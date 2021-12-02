ee3620e5732300d9df81f5978aeeb130bd64162e
youngmin <ymkwon3@gnu.ac.kr>
2021-12-03 00:06:59 +0900

    포트폴리오 제작중..

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css';
 import React, {useRef, useEffect, useState} from 'react';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import './css/App.css';
+import Project from './components/Project';
+
+// intor page
 
 function App() {
   const textRef = useRef();
   const [header, setHeader] = useState();
   const [cursor, setCursor] = useState();
   useEffect(()=>{
-    const content = "권영민의 포트폴리오페이지 입니다.";
+    const content1 = "권영민의 포트폴리오입니다.";
+    const content2 = "\n방문해주셔서 감사합니다."
     let text = "";
     let cursor = "_";
     let index = 0;
     let interval = setInterval(() => {
-      
       if(cursor === "_"){
         setCursor(cursor);
         cursor = '';
@@ -25,18 +29,44 @@ function App() {
         setCursor('');
         clearInterval(interval);
         index = 0;
+
         interval = setInterval(() => {
-          text += content[index++]
+          text += content1[index++]
           setHeader(text);
-          if(index === content.length){
+          if(index === content1.length){
             clearInterval(interval);
-            setInterval(() => {
+            index = 0;
+
+            interval = setInterval(() => {
               if(cursor === "_"){
                 setCursor(cursor);
                 cursor = '';
               } else{
                 setCursor(cursor);
                 cursor = '_'
+              }
+              index++;
+              if(index === 5){
+                setCursor('');
+                clearInterval(interval);
+                index = 0;
+
+                interval = setInterval(() => {
+                  text += content2[index++]
+                  setHeader(text);
+                  if(index === content2.length){
+                    clearInterval(interval);
+                    interval = setInterval(() => {
+                      if(cursor === "_"){
+                        setCursor(cursor);
+                        cursor = '';
+                      } else{
+                        setCursor(cursor);
+                        cursor = '_'
+                      }
+                    }, 500);
+                  }
+                }, 200);
               }
             }, 500);
           }
@@ -48,9 +78,29 @@ function App() {
   return (
     <>
       <div className="App">
-          <div className="App-header" ref={textRef}>
-            {header}{cursor}
+        <BrowserRouter>
+          <div className="App-header">
+            <div ref={textRef}>
+              {header}{cursor}
+            </div>
           </div>
+          <div className="App-aboutMe flex-row">
+            <div className="subTitle">
+              소개
+            </div>
+            <div className="line"/>
+            <div className="content flex-column">
+              좋은 개발자를 꿈꾸고 배우고 싶은 권영민입니다.
+              <br/>
+              좋은 기회 덕분에 외주를 경험하였고, 이를 통해 개발에 대한 흥미와 
+              <br/>
+              문제 해결에 대한 흥미를 느껴 좋은 개발자가 되기로 결심했습니다.
+            </div>
+          </div>
+          <div className="App-project flex-column">
+            <Project/>
+          </div>
+        </BrowserRouter>
       </div>
     </>
   );
---
+++ web/src/components/Project.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import '../css/App.css';
+import '../css/Project.css';
+function App(props) {
+    return (
+        <>
+            <div className="subTitle">
+                프로젝트
+            </div>
+            <div className="project">
+                
+            </div>
+        </>
+    );
+}
+
+export default App;
