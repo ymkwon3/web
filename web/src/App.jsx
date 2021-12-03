912672d4d5b7fe9bdd352f1d383122e047e891a8
ingmin <ymkwon3@gmail.com>
2021-12-03 13:34:42 +0900

    프로젝트 영역

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+import React, { useRef, useEffect, useState } from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './css/App.css';
 import Project from './components/Project';
@@ -10,22 +10,22 @@ function App() {
   const textRef = useRef();
   const [header, setHeader] = useState();
   const [cursor, setCursor] = useState();
-  useEffect(()=>{
+  useEffect(() => {
     const content1 = "권영민의 포트폴리오입니다.";
     const content2 = "\n방문해주셔서 감사합니다."
     let text = "";
     let cursor = "_";
     let index = 0;
     let interval = setInterval(() => {
-      if(cursor === "_"){
+      if (cursor === "_") {
         setCursor(cursor);
         cursor = '';
-      } else{
+      } else {
         setCursor(cursor);
         cursor = '_'
       }
       index++;
-      if(index === 5){
+      if (index === 5) {
         setCursor('');
         clearInterval(interval);
         index = 0;
@@ -33,20 +33,20 @@ function App() {
         interval = setInterval(() => {
           text += content1[index++]
           setHeader(text);
-          if(index === content1.length){
+          if (index === content1.length) {
             clearInterval(interval);
             index = 0;
 
             interval = setInterval(() => {
-              if(cursor === "_"){
+              if (cursor === "_") {
                 setCursor(cursor);
                 cursor = '';
-              } else{
+              } else {
                 setCursor(cursor);
                 cursor = '_'
               }
               index++;
-              if(index === 5){
+              if (index === 5) {
                 setCursor('');
                 clearInterval(interval);
                 index = 0;
@@ -54,13 +54,13 @@ function App() {
                 interval = setInterval(() => {
                   text += content2[index++]
                   setHeader(text);
-                  if(index === content2.length){
+                  if (index === content2.length) {
                     clearInterval(interval);
                     interval = setInterval(() => {
-                      if(cursor === "_"){
+                      if (cursor === "_") {
                         setCursor(cursor);
                         cursor = '';
-                      } else{
+                      } else {
                         setCursor(cursor);
                         cursor = '_'
                       }
@@ -73,8 +73,8 @@ function App() {
         }, 200);
       }
     }, 500);
-  },[]);
-  
+  }, []);
+
   return (
     <>
       <div className="App">
@@ -88,17 +88,17 @@ function App() {
             <div className="subTitle">
               소개
             </div>
-            <div className="line"/>
+            <div className="line" />
             <div className="content flex-column">
               좋은 개발자를 꿈꾸고 배우고 싶은 권영민입니다.
-              <br/>
-              좋은 기회 덕분에 외주를 경험하였고, 이를 통해 개발에 대한 흥미와 
-              <br/>
+              <br />
+              좋은 기회 덕분에 외주를 경험하였고, 이를 통해 개발에 대한 흥미와
+              <br />
               문제 해결에 대한 흥미를 느껴 좋은 개발자가 되기로 결심했습니다.
             </div>
           </div>
           <div className="App-project flex-column">
-            <Project/>
+            <Project />
           </div>
         </BrowserRouter>
       </div>
--- web/src/components/Project.jsx
+++ web/src/components/Project.jsx
@@ -7,8 +7,25 @@ function App(props) {
             <div className="subTitle">
                 프로젝트
             </div>
-            <div className="project">
-                
+            <div className="project-card flex-row" style={{ justifyContent: "space-around" }}>
+                <img src="https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E" alt="" />
+                <div className="flex-column">
+                    <div className="project-title">
+                        Project Name
+                    </div>
+                    <br />
+                    <div className="content">
+                        내용내용내용내용내용내용내용
+                        <br />
+                        내용내용내용내용내용내용내용
+                    </div>
+                </div>
+            </div>
+            <div className="project-card flex-row">
+                a
+            </div>
+            <div className="project-card flex-row">
+                a
             </div>
         </>
     );
