ca42198d9e16f93b99faa744539586c666dc7341
youngmin <ymkwon3@gnu.ac.kr>
2021-12-09 23:19:38 +0900

    was insert, delete, select check

--- web_db/dbserver.js
+++ web_db/dbserver.js
@@ -8,11 +8,23 @@ let conn = maria.createConnection({
 
 conn.connect();
 
-conn.query('select * from board', function(err, res){
+conn.query('insert into board(title, content, writer, password, date) values (?, ?, ?, ?, ?)', ['hello', 'this is content', 'Kim', '1q2w3e4r', '2021-12-09 23:09'], function(err, res){
     if(err){
         console.log("error: " + err);
     }
     console.log("res: " + res);
+});
+// conn.query('delete from board where title = \'test2\'', function(err, res){
+//     if(err){
+//         console.log("error: " + err);
+//     }
+//     console.log("res: " + res);
+// });
+conn.query('select * from board', function(err, res){
+    if(err){
+        console.log("error: " + err);
+    }
+    console.log("res: " + res[0].title + " " + res[0].content);
 });
 
 conn.end();
