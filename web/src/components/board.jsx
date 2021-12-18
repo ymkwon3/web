557e5deb1b6dd884e681850cebf1e04e135b762e
youngmin <ymkwon3@gnu.ac.kr>
2021-12-18 23:31:42 +0900

    게시물 틀 구현

--- web/src/components/board.jsx
+++ web/src/components/board.jsx
@@ -1,6 +1,9 @@
 import { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 
+import '../css/App.css';
+import '../css/Board.css';
+
 function App(props) {
     const [viewContent, setViewContent] = useState([]);
     const url = "http://175.215.49.230:3001/board";
@@ -14,10 +17,27 @@ function App(props) {
         });
     }, []);
     console.log(viewContent);
-    const boardList = viewContent.map((data, idx) => <li key={idx}>{data.title}</li>);
+    const boardList = viewContent.map((data, idx) => 
+        <li className='post flex-column' key={idx}>
+            <div className="flex-row">
+                <div className='flex-column'>
+                    <div className='post-title'>{data.title}</div>
+                    <div className='post-writer'>작성자: {data.writer}</div>
+                </div>
+                <div className='flex-row'>
+                    수정 삭제
+                </div>
+            </div>
+            <div className="post-content post-hide">
+                내용입니다.
+            </div>
+            
+            
+        </li>
+    );
 
     return (
-        <div>
+        <div className='board'>
             <ul>
                 {boardList}
             </ul>
