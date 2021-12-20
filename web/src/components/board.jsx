e6738a75052f81e3348c27b92fa189290c23d400
youngmin <ymkwon3@gnu.ac.kr>
2021-12-20 23:16:44 +0900

    게시물 삭제

--- web/src/components/board.jsx
+++ web/src/components/board.jsx
@@ -7,22 +7,42 @@ import '../css/Board.css';
 function App(props) {
     const [viewContent, setViewContent] = useState([]);
     const postRef = useRef([]);
-    const url = "http://175.215.49.230:3001/board";
+    const url = "http://175.215.49.230:3001/";
+    const apiGet = "board";
+    const apiDelete = "delete";
     
     useEffect(() => {
-        axios.get(url).then(function(res){
-            setViewContent(res.data);
-        })
-        .catch(function(err){
-            console.log("err: " + err);
-        });
+        async function getData() {
+            await axios.get(url + apiGet).then(function(res){
+                setViewContent(res.data);
+            })
+            .catch(function(err){
+                console.log("err: " + err);
+            });
+        }
+        getData();
     }, []);
 
+    // 게시물 클릭 시, 숨겨진 내용을 보여줍니다.
     const onClickHandler = (e) => {
         console.log("data : " + e);
         postRef.current[e].childNodes[1].className = postRef.current[e].childNodes[1].className === "post-content" ? "post-content post-hide" : "post-content";
         console.log(postRef.current[e].childNodes[1].className)
     }
+
+    // 게시물을 삭제합니다.
+    const onClickDelete = (e) => {
+        async function deletePost() {
+            await axios.delete(url + apiDelete + "/" + e).then(function(res){
+                console.log("게시물이 삭제되었습니다. id : " + e);
+            })
+            .catch(function(err){
+                console.log("err: " + err);
+            });
+        }
+        deletePost();
+    }
+
     console.log(viewContent);
     const boardList = viewContent.map((data) => 
         <li 
@@ -41,7 +61,7 @@ function App(props) {
                 내용입니다. {data.id}
                 <div className='flex-row'>
                     <button>수정</button>
-                    <button>삭제</button>
+                    <button onClick={() => onClickDelete(data.id)}>삭제</button>
                 </div>
             </div>
         </li>
